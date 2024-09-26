17af7f044e96ade7911963d72bb623eeed5a7113
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-09-26 04:01:56 +0200

    fix(comic): input form now working as intended

--- resources/views/partials/comics/input_form.blade.php
+++ resources/views/partials/comics/input_form.blade.php
@@ -16,7 +16,7 @@
                 TITLE
             </label>
             <input type="text" id="title" name="title" @error('title') class="error-inputted" @enderror
-                value={!! old('title', $is_update_form ? $comic_to_update->title : '""') !!} minlength="3" maxlength="125" placeholder="Title..." required>
+                value="{!! old('title', $is_update_form ? $comic_to_update->title : '') !!}" minlength="3" maxlength="125" placeholder="Title..." required>
             @error('title')
                 <small class="error-message">{{ $message }}</small>
             @enderror
@@ -27,7 +27,7 @@
                 SERIES
             </label>
             <input type="text" id="series" name="series" @error('series') class="error-inputted" @enderror
-                value={!! old('series', $is_update_form ? $comic_to_update->series : '""') !!} minlength="3" maxlength="125" placeholder="Series..." required>
+                value="{!! old('series', $is_update_form ? $comic_to_update->series : '') !!}" minlength="3" maxlength="125" placeholder="Series..." required>
             @error('series')
                 <small class="error-message">{{ $message }}</small>
             @enderror
@@ -39,7 +39,7 @@
             </label>
             <input type="number" id="issue_number" name="issue_number"
                 @error('issue_number') class="error-inputted" @enderror min="0" max="65535"
-                value={!! old('issue_number', $is_update_form ? $comic_to_update->issue_number : '""') !!} placeholder="Issue number..." required>
+                value="{!! old('issue_number', $is_update_form ? $comic_to_update->issue_number : '') !!}" placeholder="Issue number..." required>
             @error('issue_number')
                 <small class="error-message">{{ $message }}</small>
             @enderror
@@ -50,7 +50,7 @@
                 SUMMARY
             </label>
             <textarea id="summary" name="summary" @error('summary') class="error-inputted" @enderror rows="6"
-                placeholder="Summary...">{{ old('summary', $is_update_form ? $comic_to_update->summary : '') }}</textarea>
+                placeholder="Summary...">{!! old('summary', $is_update_form ? $comic_to_update->summary : '') !!}</textarea>
             @error('summary')
                 <small class="error-message">{{ $message }}</small>
             @enderror
@@ -63,7 +63,7 @@
                 COVER IMAGE
             </label>
             <input type="text" id="cover_image" name="cover_image"
-                @error('cover_image') class="error-inputted" @enderror value={!! old('cover_image', $is_update_form ? $comic_to_update->cover_image : '""') !!} maxlength="510"
+                @error('cover_image') class="error-inputted" @enderror value="{!! old('cover_image', $is_update_form ? $comic_to_update->cover_image : '') !!}" maxlength="510"
                 placeholder="Cover image URL...">
             @error('cover_image')
                 <small class="error-message">{{ $message }}</small>
@@ -75,7 +75,7 @@
                 RELEASE DATE
             </label>
             <input type="date" id="release_date" name="release_date"
-                @error('release_date') class="error-inputted" @enderror value={!! old('release_date', $is_update_form ? $comic_to_update->release_date : '""') !!}
+                @error('release_date') class="error-inputted" @enderror value="{!! old('release_date', $is_update_form ? $comic_to_update->release_date : '') !!}"
                 placeholder="Release date...">
             @error('release_date')
                 <small class="error-message">{{ $message }}</small>
@@ -87,7 +87,7 @@
                 NUMBER OF PAGES
             </label>
             <input type="number" id="page_count" name="page_count"
-                @error('page_count') class="error-inputted" @enderror value={!! old('page_count', $is_update_form ? $comic_to_update->page_count : '""') !!} min="0"
+                @error('page_count') class="error-inputted" @enderror value="{!! old('page_count', $is_update_form ? $comic_to_update->page_count : '') !!}" min="0"
                 max="65535" placeholder="Number of pages...">
             @error('page_count')
                 <small class="error-message">{{ $message }}</small>
@@ -99,7 +99,7 @@
                 PRICE
             </label>
             <input type="number" id="price" name="price" @error('price') class="error-inputted" @enderror
-                value={!! old('price', $is_update_form ? $comic_to_update->price : '""') !!} step="0.01" min="0" max="999999.99" placeholder="Price..."
+                value="{!! old('price', $is_update_form ? $comic_to_update->price : '') !!}" step="0.01" min="0" max="999999.99" placeholder="Price..."
                 required>
             @error('price')
                 <small class="error-message">{{ $message }}</small>
